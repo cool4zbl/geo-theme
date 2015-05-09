75070a5a23dbbc641197e822f438786b91c25bcb
githubzbl <goooglezbl@gmail.com>
2015-05-09 21:45:35 +0800

    Update index.html design

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,51 @@
 // index.js
-jQuery(document).ready(function($) {
-  console.log('It load', 'successful');
-});
+/**
+ * Main JS file for Index behaviours
+ */
+
+/* globals jQuery, document */
+(function ($, undefined) {
+  "use strict";
+
+  $(document).ready(function() {
+    var $postContent = $('main#content');
+    $postContent.fitVids();
+
+    $('a.scroll-down').arctic_scroll();
+
+    $('.menu-button, .nav-close').on('click', function(event) {
+      event.preventDefault();
+      $('body').toggleClass('nav-closed nav-opened');
+    });
+  });
+
+  // Arctic Scroll by Paul Adam Davis
+  // https://github.com/PaulAdamDavis/Arctic-Scroll
+  $.fn.arctic_scroll = function (options) {
+    var defaults = {
+        elem: $(this),
+        speed: 500
+    },
+
+    allOptions = $.extend(defaults, options);
+
+    allOptions.elem.click(function (event) {
+        event.preventDefault();
+        var $this = $(this),
+            $htmlBody = $('html, body'),
+            offset = ($this.attr('data-offset')) ? $this.attr('data-offset') : false,
+            position = ($this.attr('data-position')) ? $this.attr('data-position') : false,
+            toMove;
+
+        if (offset) {
+            toMove = parseInt(offset);
+            $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top + toMove) }, allOptions.speed);
+        } else if (position) {
+            toMove = parseInt(position);
+            $htmlBody.stop(true, false).animate({scrollTop: toMove }, allOptions.speed);
+        } else {
+            $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top) }, allOptions.speed);
+        }
+    });
+  };
+})(jQuery);
