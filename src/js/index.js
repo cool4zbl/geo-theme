dc8d00074a6cab02aa00fa04c046a3ca2803e40c
githubzbl <goooglezbl@gmail.com>
2015-05-10 20:43:50 +0800

    Add sidebars affix

--- src/js/index.js
+++ src/js/index.js
@@ -11,13 +11,74 @@
     var $postContent = $('main#content');
     $postContent.fitVids();
 
+    /** Move to Content **/
     $('a.scroll-down').arctic_scroll();
 
+    /** Drawer open or close **/
     $('.menu-button, .nav-close').on('click', function(event) {
       event.preventDefault();
       $('body').toggleClass('nav-closed nav-opened');
     });
+
+    /**
+    * JS: Affix
+    */
+    if ($("#navigation").length){
+      $("#navigation").affix({
+        offset: {
+          top: $('#content').offset().top-20,
+          bottom: function () {
+            return (this.bottom = $('footer.site-footer').outerHeight(true));
+          }
+        }
+      });
+    }
+
+    if ($("#share").length){
+      $("#share").affix({
+        offset: {
+          top: $('#post-container').offset().top-20,
+          bottom: function () {
+            return (this.bottom = $('footer.site-footer').outerHeight(true));
+          }
+        }
+      });
+    }
+
+
+    /**
+    * JS: Progress bar
+    */
+
+    $(window).scroll(function () {
+      if ($("#progressbar").length){
+          var s = $(window).scrollTop(),
+          d = $(document).height(),
+          c = $(window).height(),
+          e = $(".post-footer").height(),
+          f = parseInt($(".post-footer").css("margin-top")) + parseInt($(".post-footer").css("padding-top")),
+          g = $(".site-footer").height(),
+          h = parseInt($(".site-footer").css("margin-top")) + parseInt($(".site-footer").css("padding-top")),
+          i = $("#disqus_thread").height();
+
+          var scrollPercent = (s / (d-c-e-f-g-h-i)) * 100;
+          scrollPercent = Math.round(scrollPercent);
+
+          if (c >= (d-e-f-g-h-i)) {
+            scrollPercent = 100;
+          } else if (scrollPercent < 0) {
+            scrollPercent = 0;
+          } else if (scrollPercent > 100) {
+            scrollPercent = 100;
+          }
+          $("#progressbar").css("width", scrollPercent + "%");
+          $("#progressbar-value").html(scrollPercent + "%");
+      }
+
+    });
   });
+
+  // jQuery Plugins
 
   // Arctic Scroll by Paul Adam Davis
   // https://github.com/PaulAdamDavis/Arctic-Scroll
